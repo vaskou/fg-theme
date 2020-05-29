34b2c6b6a91dfeed0ccff4fb7e0516677f7b1d1f
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-05-29 19:49:47 +0300

    -WIP

--- template-parts/content-fg_guitars.php
+++ template-parts/content-fg_guitars.php
@@ -2,16 +2,20 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="fg-box uk-text-center">
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'uk-flex uk-child-width-1-1' ); ?>>
+    <div class="fg-box uk-text-center uk-flex uk-child-width-1-1 uk-flex-right uk-flex-column">
 		<?php
 		$link     = get_permalink();
 		$image_id = FG_Guitars_Images_Fields::getInstance()->getMenuImageID( get_the_ID() );
 
+		if ( ! empty( $image_id ) ):
+			?>
+            <a href="<?php echo $link; ?>" class="uk-display-block ">
+				<?php echo wp_get_attachment_image( $image_id, 'full' ); ?>
+            </a>
+		<?php
+		endif;
 		?>
-        <a href="<?php echo $link; ?>" class="uk-display-block ">
-			<?php echo wp_get_attachment_image( $image_id, 'full' ); ?>
-        </a>
         <a href="<?php echo $link; ?>" class="uk-display-block ">
 			<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
         </a>
