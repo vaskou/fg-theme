b71c542aadf533453bdb7c155b12d7871c8464e9
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-06-05 18:47:19 +0300

    -WIP

--- index.php
+++ index.php
@@ -16,48 +16,48 @@ get_header();
 ?>
 
     <div id="primary" class="content-area">
-    <div id="main" class="site-main">
+        <main id="main" class="site-main">
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-                <header>
-                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                </header>
 			<?php
+			if ( have_posts() ) :
+
+				if ( is_home() && ! is_front_page() ) :
+					?>
+                    <header>
+                        <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                    </header>
+				<?php
+				endif;
+				?>
+                <div class="uk-grid uk-child-width-1-2@m" uk-grid>
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) :
+						the_post();
+
+						/*
+						 * Include the Post-Type-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_type() );
+
+					endwhile;
+					?>
+                </div>
+				<?php
+				the_posts_pagination( array(
+					'type'      => 'list',
+					'prev_text' => '<span uk-pagination-previous></span>',
+					'next_text' => '<span uk-pagination-next></span>',
+				) );
+
+			else :
+
+				get_template_part( 'template-parts/content', 'none' );
+
 			endif;
 			?>
-            <div class="uk-grid uk-child-width-1-2@m" uk-grid>
-				<?php
-				/* Start the Loop */
-				while ( have_posts() ) :
-					the_post();
-
-					/*
-					 * Include the Post-Type-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					 */
-					get_template_part( 'template-parts/content', get_post_type() );
-
-				endwhile;
-				?>
-            </div>
-			<?php
-			the_posts_pagination( array(
-				'type'      => 'list',
-				'prev_text' => '<span uk-pagination-previous></span>',
-				'next_text' => '<span uk-pagination-next></span>',
-			) );
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
 
         </main><!-- #main -->
     </div><!-- #primary -->
