3d46e1488d128c10021325e6b08421a25e4e83a9
Vasilis Koutsopoulos <vaskou@hotmail.com>
2021-05-16 11:47:51 +0300

    -WIP

---
+++ inc/class-fremediti-guitars-fg-prices.php
@@ -0,0 +1,29 @@
+<?php
+
+class Fremediti_Guitars_FG_Prices {
+
+	private static $_instance;
+
+	public static function instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	private function __construct() {
+		add_filter( 'fg_prices_get_multicurrency_prices', array( $this, 'get_multicurrency_prices' ), 10, 2 );
+	}
+
+	public function get_multicurrency_prices( $price, $multicurrency_prices ) {
+
+		$current_currency = '';
+
+		if ( ! empty( $multicurrency_prices[ $current_currency ] ) ) {
+			$price = $multicurrency_prices[ $current_currency ];
+		}
+
+		return $price;
+	}
+}
--- template-parts/content-fg_pickups.php
+++ template-parts/content-fg_pickups.php
@@ -1,8 +1,6 @@
 <?php
 $template = ! empty( $args['template'] ) ? $args['template'] : '';
 
-Fremediti_Guitars_FG_Pickups::instance();
-
 if ( 'grid' == $template ) {
 	get_template_part( 'template-parts/content', 'fg_pickups-grid' );
 } else {
