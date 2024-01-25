9b401d3539478b5b19c66a1023d39a1e88dba597
Vasilis Koutsopoulos <vaskou@hotmail.com>
2024-01-25 19:36:02 +0200

    -WIP

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'FREMEDITI_GUITARS_THEME_VERSION', '1.1.8' );
+define( 'FREMEDITI_GUITARS_THEME_VERSION', '1.2.0' );
 define( 'FREMEDITI_GUITARS_THEME_PATH', get_stylesheet_directory() );
 define( 'FREMEDITI_GUITARS_THEME_URL', get_stylesheet_directory_uri() );
 
